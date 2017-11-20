c72ae4eea5729d4abe95573e3c52ec2140e2ae31
legale <legale.legale@gmail.com>
2017-11-20 13:27:20 +0100

    rapida 0.0.8.1.4

--- sandbox/sandbox.php
+++ sandbox/sandbox.php
@@ -7,9 +7,18 @@ if (isset($_SESSION['admin'])) {
 
 	print "<PRE>";
 
-	$simpla->cache->set_cache_nosql('123', array(1,2,3,4,'пять'));
-	$var = $simpla->cache->get_cache_nosql('123');
-	print_r($var);
+	//~ $simpla->cache->set_cache_nosql('123', array(1,2,3,4,'пять'));
+	//~ $var = $simpla->cache->get_cache_nosql('123');
+	//~ print_r($var);
+
+
+
+			$f = $simpla->features->get_features_ids(array('in_filter'=>1, 'return' => 
+			array('key' => 'id', 'col' => 'id')
+			)
+			);
+	var_dump($f);
+
 	print "</PRE>";
 	dtimer::show();
 }
